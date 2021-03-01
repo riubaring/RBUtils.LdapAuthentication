0b4a5926194cb007b65bd96100213f0cfcbc8f38
Riu Baring <riubaring@gmail.com>
2021-03-01 00:06:56 -0800

    Typo

--- RBUtils.LdapAuthentication.Core/LogInResult.cs
+++ RBUtils.LdapAuthentication.Core/LogInResult.cs
@@ -4,9 +4,9 @@ namespace RBUtils.LdapAuthentication.Core
 {
     public class LogInResult
     {
-        public static readonly LogInResult _success = new LogInResult { Succeeded = true };
-        public static readonly LogInResult _failed = new LogInResult();
-        public static readonly LogInResult _notAllowed = new LogInResult { IsNotAllowed = true };
+        private static readonly LogInResult _success = new LogInResult { Succeeded = true };
+        private static readonly LogInResult _failed = new LogInResult();
+        private static readonly LogInResult _notAllowed = new LogInResult { IsNotAllowed = true };
 
         public bool Succeeded { get; protected set; }
         public bool IsNotAllowed { get; protected set; }
@@ -14,5 +14,11 @@ namespace RBUtils.LdapAuthentication.Core
         public static LogInResult Success => _success;
         public static LogInResult Failed => _failed;
         public static LogInResult NotAllowed => _notAllowed;
+
+        public override string ToString()
+        {
+            return IsNotAllowed ? "NotAllowed" :
+                Succeeded ? "Succeeded" : "Failed";
+        }
     }
 }
