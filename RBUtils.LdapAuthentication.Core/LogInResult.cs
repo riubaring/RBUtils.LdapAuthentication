e92eef1e66df9227a5a92e50fe14546c7e5f4a2f
Riu Baring <riubaring@gmail.com>
2021-02-28 20:55:17 -0800

    Added properties and methods.

--- RBUtils.LdapAuthentication.Core/LogInResult.cs
+++ RBUtils.LdapAuthentication.Core/LogInResult.cs
@@ -4,5 +4,15 @@ namespace RBUtils.LdapAuthentication.Core
 {
     public class LogInResult
     {
+        public static readonly LogInResult _success = new LogInResult { Succeeded = true };
+        public static readonly LogInResult _failed = new LogInResult();
+        public static readonly LogInResult _notAllowed = new LogInResult { IsNotAllowed = true };
+
+        public bool Succeeded { get; protected set; }
+        public bool IsNotAllowed { get; protected set; }
+
+        public static LogInResult Success => _success;
+        public static LogInResult Faile => _failed;
+        public static LogInResult NotAllowed => _notAllowed;
     }
 }
