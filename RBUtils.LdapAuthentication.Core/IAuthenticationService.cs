adeeac2a1d4046b4fb8aece1896d07baa5b99330
Riu Baring <riubaring@gmail.com>
2021-02-28 21:01:22 -0800

    Re-created IAuthenticationService

---
+++ RBUtils.LdapAuthentication.Core/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RBUtils.LdapAuthentication.Core
+{
+    public interface IAuthenticationService
+    {
+        HttpContent HttpContext { get; set; }
+
+        Task<LogInResult> LogInAsync(string userName, string plainPassword, bool isPersistent);
+        Task LogOut();
+    }
+}
