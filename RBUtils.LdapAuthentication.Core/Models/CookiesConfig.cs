6632acd2afcf6668c038be5132e655db645b979b
Riu Baring <riubaring@gmail.com>
2021-02-28 21:07:19 -0800

    Re-create IAuthenticationService

---
+++ RBUtils.LdapAuthentication.Core/Models/CookiesConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RBUtils.LdapAuthentication.Core.Models
+{
+    public class CookiesConfig
+    {
+        public string CookieName { get; set; }
+        public string LoginPath { get; set; }
+        public string LogoutPath { get; set; }
+        public string AccessDeniedPath { get; set; }
+        public string ReturnUrlParameter { get; set; }
+    }
+}
---
+++ RBUtils.LdapAuthentication.Core/Models/LdapConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RBUtils.LdapAuthentication.Core.Models
+{
+    public class LdapConfig
+    {
+        public string Path { get; set; }
+        public string UserDomainName { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
