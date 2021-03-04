47cdf490bebe5abd61ef5a108ee615ac7e830cd1
Riu Baring <riubaring@gmail.com>
2021-03-03 17:17:41 -0800

    Added a wrapper class that takes care of a. Reads Ldap config from Configuration (appsettings.json) and add its the service b. Reads Cookie config from Configuration (appsettings.json) Cookie authentication c. Makes sure HttpContextAccessor is added d. Addes Authentication Service

---
+++ RBUtils.LdapAuthentication.Core/AuthenticationServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RBUtils.LdapAuthentication.Core.Models;
+using System;
+using Microsoft.Extensions.Options.ConfigurationExtensions;
+
+namespace RBUtils.LdapAuthentication.Core
+{
+    public static class AuthenticationServiceExtensions
+    {
+        public static void AddLdapAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Load Ldap configuration. Use default, if null
+            services.Configure<LdapConfig>(configuration.GetSection("Ldap"));
+
+            // Load Cookie configuration. Use default, if null
+            var cookieConfig = configuration.GetSection("Cookie").Get<CookiesConfig>();
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                 {
+                     options.Cookie.Name = cookieConfig.CookieName;
+                     options.LoginPath = cookieConfig.LoginPath;
+                     options.LogoutPath = cookieConfig.LogoutPath;
+                     options.AccessDeniedPath = cookieConfig.AccessDeniedPath;
+                     options.ReturnUrlParameter = cookieConfig.ReturnUrlParameter;
+                 });
+
+            // Make sure IHttpContextAccessor is added
+            services.AddHttpContextAccessor();
+
+            // Add the AuthenticationService
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+        }
+    }
+}
